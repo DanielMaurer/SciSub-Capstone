f1d0f3db589b48eb4414797d3ddf9207009dcfe1
DanielMaurer <danielrmaurer10@gmail.com>
2019-04-15 10:41:34 -0500

    Takes data based on user input. Need to adjust python code to start this function

--- Arduino Code/testArduinoData/testArduinoData.ino
+++ Arduino Code/testArduinoData/testArduinoData.ino
@@ -1,28 +1,40 @@
 int dtime = 0;
-int temperature = 0;
+
+float temperature = 0.00;
+const int temperatureSensor = A0;
+
 int pressure = 0;
 int turbidity = 0;
+
+char userTimeInput;
+
+void takeData(){
+  Serial.println("Time (s)\tTemperature (°C)\tPressure\tTurbidity\n");
+  while(dtime < 20){
+    dtime+=1;
+    Serial.print(dtime);
+    Serial.print('\t');
+    temperature = (0.4868 * analogRead(temperatureSensor)) - 49.772;  
+    Serial.print(temperature);
+    Serial.print('\t');
+    pressure+=1;
+    Serial.print(pressure);
+    Serial.print('\t');
+    turbidity+=1;
+    Serial.print(turbidity);
+    Serial.print('\n');
+    delay(1000);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Time\tTemperature\tPressure\tTurbidity\n");
 }
 
 void loop() {
-  dtime+=1;
-  Serial.print(dtime);
-  Serial.print('\t');
-  temperature+=1;
-  Serial.print(temperature);
-  Serial.print('\t');
-  pressure+=1;
-  Serial.print(pressure);
-  Serial.print('\t');
-  turbidity+=1;
-  Serial.print(turbidity);
-  Serial.print('\n');
-  //Serial.println(dtime + '\t' + temperature + '\t' + pressure + '\t' + turbidity + '\n');
-  delay(1000);
-  
+  userTimeInput = Serial.read();
+  if(userTimeInput == 's'){
+    takeData();
+  }
 }
